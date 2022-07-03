06891999150d06d3ce3dbf8217cb5485ca94d33f
Artur Coelho <artur.rocha.coelho@gmail.com>
2022-07-03 19:51:26 -0300

    feat: input component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,11 @@
 import Button from './Button';
 import BaseCard from './BaseCard';
+import Input from './Input';
 
 const Login = () => {
   return (
     <BaseCard>
+      <Input label='Senha' type='password' />
       <Button bgColor='#02274F' text='Teste' />
     </BaseCard>
   );

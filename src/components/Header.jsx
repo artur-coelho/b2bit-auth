1b886a80aebf624131694cd80e62a7f1a5bcef55
Artur Coelho <artur.rocha.coelho@gmail.com>
2022-07-04 20:23:57 -0300

    feat: created header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Button from './Button';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <Button text='Logout' width='272px' />
+    </header>
+  );
+};
+
+export default Header;
